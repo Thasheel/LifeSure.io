d97315de3e68931656fe35e5a6e69b20e17b12e4
thasheel <mohdthasheelok@gmail.com>
2024-08-07 14:11:13 +0530

    read done successfully

--- rootapp/urls.py
+++ rootapp/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('home',views.home,name='home'),
     path('index',views.index,name='index'),
     path('dash',views.dash,name='dash'),
-    path('submit',views.submit,name='submit')
+    path('submit',views.submit,name='submit'),
+    path('viewdata', views.viewdata, name='viewdata'),
 ]
--- rootapp/views.py
+++ rootapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 from root.forms import InsuranceForm
+from rootapp.models import LifeInsurance
 
 
 # Create your views here.
@@ -15,4 +16,13 @@ def dash(request):
     return render(request,'dash.html')
 def submit(request):
     form = InsuranceForm()
+    if request.method == 'POST':
+        data=InsuranceForm(request.POST)
+        if data.is_valid():
+            data.save()
     return render(request,'submit.html',{'form':form})
+
+def viewdata(request):
+    info= LifeInsurance.objects.all()
+    return render(request, 'read.html', {'info': info})
+
